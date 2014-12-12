27646f2e9f5ddee0dad1eafe75b0c9b9a279f35f
theonlynexus <theonlynexus@75f985d5-eef5-437a-b917-4a5afe5ebf4a>
2014-12-12 15:47:51 +0000

    Reworked forum to work with CI's templates. TODO: Fix the silly replies count not showing correctly -.-

--- install/www_root/application/views/fud/forum.php
+++ install/www_root/application/views/fud/forum.php
@@ -1,48 +1,30 @@
+    {navigation}
 
+    {pagination}
 
-<?php echo $navigation ?>
-
-<?php echo $pagination ?>
-
-<table id="topics_table" class="fud_table" border="0" cellspacing="1" cellpadding="2" >
-    <thead class="table_header">
+    <table id="topics_table" class="fud_table" border="0" cellspacing="1" cellpadding="2" >
+      <thead class="table_header">
         <th class="th topic">Topic</th>
         <th class="th replies">Replies</th>
         <th class="th views">Views</th>
         <th class="th last_message">Last message</th>
-    </thead>
-    <tbody>
-<?php
-    for( $i=0; $i<count($topics); ++$i )
-    {
-        $topic = $topics[$i];
+      </thead>
+      <tbody>
+      {topics}
+        <tr>
+          <td class="topic">
+            <div>
+              <div class="width_100"><a href="{t_url}">{t_subject}</a></div>
+              <div class="width_100 inline_block">{t_description}</div>
+              <div class="width_100 author_and_date"><span class="author">by {t_author}</span> <span class="date">on {t_date}</span></div>
+            </div>
+          </td>
+          <td class="replies">{t_replies}</td>
+          <td class="views">{t_views}</td>
+          <td class="last_message"><span class="date">{t_last_date}</span><br/><span class="author">by {t_last_author}</span></td>
+        </tr>
+      {/topics}
+      </tbody>
+    </table>
 
-        $author = $topic->root_message->login;
-        $desc = $topic->tdescr;
-        $date = date( "D, j F Y", $topic->root_message->post_stamp );
-        $last_author = $topic->last_message->login;
-        $last_date = date( "D, j F Y H:m", $topic->last_message->post_stamp );
-		$topic_url = site_url( "topic/{$cid}/{$fid}/{$topic->topic_id}" );
-
-        $out = "        " .
-               "<tr >" .
-               "<td class=\"topic\">".
-               "<div>".
-               "<div class=\"width_100\"><a href=\"{$topic_url}\">{$topic->subject}</a></div>".
-               "<div class=\"width_100 inline_block\">{$desc}</div>".
-               "<div class=\"width_100 author_and_date\"><span class=\"author\">by {$author}</span> <span class=\"date\">on {$date}</span></div>" .
-               "</div>" .
-               "</td>" .
-               "<td class=\"replies\">{$topic->replies}</td>" .
-               "<td class=\"views\">{$topic->views}</td>" .
-               "<td class=\"last_message\"><span class=\"date\">{$last_date}</span><br/><span class=\"author\">by {$last_author}</span></td>" .
-               "</tr>" .
-               "\n";
-        echo $out;
-    }
-?>
-    </tbody>
-</table>
-
-<?php echo $pagination ?>
-
+    {pagination}
