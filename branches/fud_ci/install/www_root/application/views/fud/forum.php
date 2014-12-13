8d193524745c30698d1febe3fbf528fc0a77578b
theonlynexus <theonlynexus@75f985d5-eef5-437a-b917-4a5afe5ebf4a>
2014-12-13 15:29:36 +0000

    Improved the fix to CI's parser system by leveraging the thrown exception, thus fixing the problem with the t_replies field in the forum view

--- branches/fud_ci/install/www_root/application/views/fud/forum.php
+++ branches/fud_ci/install/www_root/application/views/fud/forum.php
@@ -16,12 +16,18 @@
             <div>
               <div class="width_100"><a href="{t_url}">{t_subject}</a></div>
               <div class="width_100 inline_block">{t_description}</div>
-              <div class="width_100 author_and_date"><span class="author">by {t_author}</span> <span class="date">on {t_date}</span></div>
+              <div class="width_100 author_and_date">
+                <span class="author">by {t_author}</span>
+                <span class="date">on {t_date}</span>
+              </div>
             </div>
           </td>
           <td class="replies">{t_replies}</td>
           <td class="views">{t_views}</td>
-          <td class="last_message"><span class="date">{t_last_date}</span><br/><span class="author">by {t_last_author}</span></td>
+          <td class="last_message">
+            <span class="date">{t_last_date}</span><br/>
+            <span class="author">by {t_last_author}</span>
+          </td>
         </tr>
       {/topics}
       </tbody>
