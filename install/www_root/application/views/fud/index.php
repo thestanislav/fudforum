f7bd84d6ae5134b729ef51d0db871dbfd009d5df
theonlynexus <theonlynexus@75f985d5-eef5-437a-b917-4a5afe5ebf4a>
2015-01-09 04:32:32 +0000

    Added script for toggling categories

--- install/www_root/application/views/fud/index.php
+++ install/www_root/application/views/fud/index.php
@@ -3,6 +3,21 @@
     <!-- Navigation -->
     {path_navigation}
     <table id="fora_table" class="fud_table" border="0" cellspacing="1" cellpadding="2">
+        <script>
+          // TODO(nexus): move in common script file (?)
+          $().ready( function() {
+            $(".toggler").click( function() {
+              var id =  this.id;  
+              if( $(this).html() == "-" ) {
+                $(".cat_"+id.slice(0,id.length-8)+"_child").slideUp();
+                $(this).html("+");
+              } else {
+                $(".cat_"+id.slice(0,id.length-8)+"_child").slideDown();
+                $(this).html("-");
+              }
+            })
+          });
+        </script>
         <thead class="table_header" >
             <th class="th forum">Forum</th>
             <th class="th messages">Messages</th>
@@ -11,13 +26,13 @@
         </thead>
         <tbody>
         {categories}
-          <tr>
+          <tr id="cat_{c_id}">
             <td class="category" colspan="4" >
-              <a href="{c_url}">{c_name}</a> {c_description}
+              <span id="{c_id}_toggler" class="toggler pure-button">-</span><a href="{c_url}">{c_name}</a> {c_description}
             </td>
           </tr>
           {forums}
-            <tr>
+            <tr class="cat_{c_id}_child">
               <td class="forum"><a href="{f_url}">{f_name}</a><br/>{f_description}</td>
               <td class="messages">{f_post_count}</td>
               <td class="topics">{f_thread_count}</td>
