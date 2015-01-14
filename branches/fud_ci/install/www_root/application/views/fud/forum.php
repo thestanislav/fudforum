5273e12aef72956d20d900f80b5fbe3a9b598687
theonlynexus <theonlynexus@75f985d5-eef5-437a-b917-4a5afe5ebf4a>
2015-01-14 19:14:12 +0000

    Reworked layout and simplified CSS (to be improved later).

--- branches/fud_ci/install/www_root/application/views/fud/forum.php
+++ branches/fud_ci/install/www_root/application/views/fud/forum.php
@@ -1,49 +1,45 @@
-    <div id="wrapper-grid" class="pure-g pure-skin-fud">
-      <div class="pure-u-1-24 pure-skin-fud">&nbsp;</div>
-      <div id="wrapper-unit" class="pure-u-22-24 pure-skin-fud">          
-        <!-- Header -->
-        {header}
-        <!-- Menu -->
-        {site_navigation}
-        <!-- Navigation -->
-        {path_navigation}
-        <!-- Pagination -->
-        {pagination}
-        <!-- Contents table -->
-        <table id="topics_table" 
-                class="fud_table pure-table pure-table-striped pure-skin-fud" 
-                border="0" cellspacing="1" cellpadding="2" >
-          <thead class="table_header">
-            <th class="th topic fud_first_col">Topic</th>
-            <th class="th replies">Replies</th>
-            <th class="th views">Views</th>
-            <th class="th last_message">Last message</th>
-          </thead>
-          <tbody>
-          {topics}
-            <tr>
-              <td class="topic fud_first_col">
-                <div>
-                  <div class="width_100"><a href="{t_url}">{t_subject}</a></div>
-                  <div class="width_100 inline_block">{t_description}</div>
-                  <div class="width_100 author_and_date">
-                    <span class="author">by {t_author}</span>
-                    <span class="date">on {t_date}</span>
-                  </div>
-                </div>
-              </td>
-              <td class="replies">{t_replies}</td>
-              <td class="views">{t_views}</td>
-              <td class="last_message">
-                <span class="date">{t_last_date}</span><br/>
-                <span class="author">by {t_last_author}</span>
-              </td>
-            </tr>
-          {/topics}
-          </tbody>
-        </table>
-        <!-- Pagination -->
-        {pagination}
-      </div>
-      <div class="pure-u-1-24 pure-skin-fud">&nbsp;</div>
-    </div>
+        <div id="table_wrapper" class="pure-g">
+          <div class="pure-u-1-24">&nbsp;</div>
+          <div class="pure-u-22-24">
+            <!-- Navigation -->
+            {path_navigation}
+            <!-- Pagination -->
+            {pagination}
+            <!-- Contents table -->
+            <table id="topics_table" 
+                    class="fud_table pure-table pure-table-striped pure-skin-fud" 
+                    border="0" cellspacing="1" cellpadding="2" >
+              <thead class="table_header">
+                <th class="th topic fud_first_col">Topic</th>
+                <th class="th replies">Replies</th>
+                <th class="th views">Views</th>
+                <th class="th last_message">Last message</th>
+              </thead>
+              <tbody>
+              {topics}
+                <tr>
+                  <td class="topic fud_first_col">
+                    <div>
+                      <div class="width_100"><a href="{t_url}">{t_subject}</a></div>
+                      <div class="width_100 inline_block">{t_description}</div>
+                      <div class="width_100 author_and_date">
+                        <span class="author">by {t_author}</span>
+                        <span class="date">on {t_date}</span>
+                      </div>
+                    </div>
+                  </td>
+                  <td class="replies">{t_replies}</td>
+                  <td class="views">{t_views}</td>
+                  <td class="last_message">
+                    <span class="date">{t_last_date}</span><br/>
+                    <span class="author">by {t_last_author}</span>
+                  </td>
+                </tr>
+              {/topics}
+              </tbody>
+            </table>
+            <!-- Pagination -->
+            {pagination}
+          </div>
+          <div class="pure-u-1-24 pure-skin-fud">&nbsp;</div>
+        </div>
