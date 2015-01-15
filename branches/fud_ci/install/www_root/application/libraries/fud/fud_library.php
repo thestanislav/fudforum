bbd43d407e0b84febff36b4ff48375953ca00cf2
theonlynexus <theonlynexus@75f985d5-eef5-437a-b917-4a5afe5ebf4a>
2015-01-15 04:38:26 +0000

    Functional reply with optional quote and CKEditor. TODO: parity with trunk

--- branches/fud_ci/install/www_root/application/libraries/fud/fud_library.php
+++ branches/fud_ci/install/www_root/application/libraries/fud/fud_library.php
@@ -1,16 +1,22 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+  * Wrapper for the scripts/fudapi.php library with some bonuses
+  *
+  * @author  Massimo Fierro <massimo.fierro@gmail.com>
+  */
+
 class Fud_Library
 {
     //private static $instance;
 
     public function __construct()
     {
-        $this->CI = &get_instance();
-        $this->CI->load->helper( 'fud' );
-        require_once 'GLOBALS.php';
-    	require_once $GLOBALS['DATA_DIR'].'/scripts/fudapi.inc.php';
-        require_once $GLOBALS['DATA_DIR'].'/scripts/forum_login.php';
+      $this->CI = &get_instance();
+      $this->CI->load->helper( 'fud' );
+      require_once 'GLOBALS.php';
+      require_once $GLOBALS['DATA_DIR'].'/scripts/fudapi.inc.php';
+      require_once $GLOBALS['DATA_DIR'].'/scripts/forum_login.php';
     }
 
     function get_uid_from_login($login)
@@ -23,96 +29,103 @@ class Fud_Library
     	return external_get_user_by_auth( $login, $passwd );
     }
 
-    function login( $uid )
+  function login( $uid )
+  {
+    return external_fud_login( $uid );
+  }
+
+  function logout( $uid )
+  {
+    return external_fud_logout( $uid );
+  }
+
+  function fetch_message( $mid )
+  {
+    return fud_fetch_msg( $mid );
+  }
+
+  function fetch_topic_info( $tid )
+  {
+    return fud_fetch_topic( $tid );
+  }
+
+  function fetch_full_topic( $tid )
+  {
+    return fud_fetch_full_topic( $tid );
+  }
+
+  function fetch_topics_by_forum( $fid, $sort = FALSE, $limit = null )
+  {
+    return fud_fetch_forum_topics( $fid, $sort, $limit );
+  }
+
+  function fetch_categories( $catIds = null, $sort = FALSE )
+  {
+    return fud_fetch_cat( $catIds, $sort );
+  }
+
+  function fetch_forums_by_category( $categories = null, $sort = FALSE )
+  {
+    return fud_fetch_cat_forums( $categories, $sort );
+  }
+
+  function fetch_forums( $frmId = null, $sort = FALSE )
+  {
+    return fud_fetch_forum( $frmId, $sort );
+  }
+
+  function new_reply( $subject, $body, $mode, $author, $rep_id, $icon=null, $attach=null, $poll=null, $time=null )
+  {
+    return fud_new_reply($subject, $body, $mode, $author, $rep_id, $icon, $attach, $poll, $time);
+  }
+
+  function add_user($vals, &$err)
+  {
+    return fud_add_user($vals, $err);
+  }
+
+  function update_user($vals, &$err)
+  {
+    return fud_update_user($vals, $err);
+  }
+
+  function check_permissions( $rid, $uid = 0 )
+  {
+    $prefix = $GLOBALS['DBHOST_TBL_PREFIX'];
+    $id2 = $uid ? 2147483647 : 0;
+
+    $joins = "";
+    if( $uid )
     {
-    	return external_fud_login( $uid );
+      $joins = " LEFT JOIN {$prefix}mod AS mo \n" .
+                "   ON mo.user_id='{$uid}' AND mo.forum_id='{$rid}' \n".
+                " LEFT JOIN {$prefix}group_cache AS g2 \n".
+                "   ON g2.user_id='{$uid}' AND g2.resource_id='{$rid}' \n".
+                " LEFT JOIN {$prefix}users AS usr \n ".
+                "   ON usr.id='{$uid}' ";
     }
-
-    function logout( $uid )
+                  
+    $selects = " g1.group_cache_opt ";
+    if( $uid )
     {
-    	return external_fud_logout( $uid );
+      $selects = "usr.users_opt, mo.id AS is_mod, 
+                  COALESCE(g2.group_cache_opt, g1.group_cache_opt) 
+                  AS group_cache_opt ";
     }
+    
+    $qStr = " SELECT {$selects}
+              FROM {$prefix}group_cache AS g1
+              {$joins}
+              WHERE ( g1.user_id='{$id2}' AND g1.resource_id='{$rid}' ) ";
+    $q = $this->CI->db->query( $qStr );
 
-    function fetch_message( $mid )
+    $t = new FUD_groups_opt();
+    $r = new ReflectionObject($t);
+    $constants = $r->getConstants();
+
+    $permissions = array();
+    if( $q->num_rows() == 1 )
     {
-    	return fud_fetch_msg( $mid );
-    }
-
-    function fetch_topic_info( $tid )
-    {
-		return fud_fetch_topic( $tid );
-	}
-
-    function fetch_full_topic( $tid )
-    {
-    	return fud_fetch_full_topic( $tid );
-    }
-
-    function fetch_topics_by_forum( $fid, $sort = FALSE, $limit = null )
-    {
-        return fud_fetch_forum_topics( $fid, $sort, $limit );
-    }
-
-    function fetch_categories( $catIds = null, $sort = FALSE )
-    {
-        return fud_fetch_cat( $catIds, $sort );
-    }
-
-    function fetch_forums_by_category( $categories = null, $sort = FALSE )
-    {
-		    return fud_fetch_cat_forums( $categories, $sort );
-    }
-
-    function fetch_forums( $frmId = null, $sort = FALSE )
-    {
-        return fud_fetch_forum( $frmId, $sort );
-    }
-
-    function new_reply( $subject, $body, $mode, $author, $rep_id, $icon=null, $attach=null, $poll=null, $time=null )
-    {
-    	return fud_new_reply($subject, $body, $mode, $author, $rep_id, $icon, $attach, $poll, $time);
-    }
-
-    function add_user($vals, &$err)
-    {
-    	return fud_add_user($vals, $err);
-    }
-
-    function update_user($vals, &$err)
-    {
-    	return fud_update_user($vals, $err);
-    }
-
-    function check_permissions( $rid, $uid = 0 )
-    {
-		$id2 = $uid ? 2147483647 : 0;
-
-        $joins = "";
-        if( $uid )
-            $joins = " LEFT JOIN fud30_mod AS mo \n" .
-                     "   ON mo.user_id='{$uid}' AND mo.forum_id='{$rid}' \n".
-                     " LEFT JOIN fud30_group_cache AS g2 \n".
-                     "   ON g2.user_id='{$uid}' AND g2.resource_id='{$rid}' \n".
-                     " LEFT JOIN fud30_users AS usr \n ".
-                     "   ON usr.id='{$uid}' ";
-
-        $selects = " g1.group_cache_opt ";
-        if( $uid )
-            $selects = " usr.users_opt, mo.id AS is_mod, COALESCE(g2.group_cache_opt, g1.group_cache_opt) AS group_cache_opt ";
-
-        $qStr = " SELECT {$selects} \n".
-                " FROM fud30_group_cache AS g1 \n".
-                " {$joins} \n" .
-                " WHERE ( g1.user_id  = '{$id2}' AND g1.resource_id = '{$rid}' ) \n";
-        $q = $this->CI->db->query( $qStr );
-
-        $t = new FUD_groups_opt();
-        $r = new ReflectionObject($t);
-        $constants = $r->getConstants();
-
-        $permissions = array();
-        if( $q->num_rows() == 1 )
-        {
 			$r = $q->first_row();
 			$perms = array_keys( $constants );
 
@@ -128,81 +141,97 @@ class Fud_Library
 			return $permissions;
 		}
 
-        return FALSE;
+    return FALSE;
 	}
 
-    function check_permission( $rid, $uid = 0, $permission )
+  function check_permission( $rid, $uid = 0, $permission )
+  {
+    $prefix = $GLOBALS['DBHOST_TBL_PREFIX'];
+    if( !is_numeric( $permission ) )
     {
-		if( !is_numeric( $permission ) )
-			return FALSE;
-
-		$id2 = $uid ? 2147483647 : 0;
-
-        $joins = "";
-        if( $uid )
-            $joins = " LEFT JOIN fud30_mod AS mo \n" .
-                     "   ON mo.user_id='{$uid}' AND mo.forum_id='{$rid}' \n".
-                     " LEFT JOIN fud30_group_cache AS g2 \n".
-                     "   ON g2.user_id='{$uid}' AND g2.resource_id='{$rid}' \n".
-                     " LEFT JOIN fud30_users AS usr \n ".
-                     "   ON usr.id='{$uid}' ";
-
-        $selects = " g1.group_cache_opt ";
-        if( $uid )
-            $selects = " usr.users_opt, mo.id AS is_mod, COALESCE(g2.group_cache_opt, g1.group_cache_opt) AS group_cache_opt ";
-
-        $qStr = " SELECT {$selects} \n".
-                " FROM fud30_group_cache AS g1 \n".
-                " {$joins} \n" .
-                " WHERE ( g1.user_id  = '{$id2}' AND g1.resource_id = '{$rid}' ) \n";
-        $q = $this->CI->db->query( $qStr );
-
-        if( count( $q->result_array() ) == 1 )
-        {
-            $r = $q->row();
-            if( ( isset($r->users_opt) AND ($r->users_opt & FUD_users_opt::IS_ADMIN) ) OR
-                ( isset($r->is_mod) AND null != $r->is_mod) OR
-                ($r->group_cache_opt & $permission) )
-                return TRUE;
-        }
-
-        return FALSE;
-	}
-
-    function forum_is_visible( $rid, $uid = 0 )
-    {
-        $id2 = $uid ? 2147483647 : 0;
-
-        $joins = "";
-        if( $uid )
-            $joins = " LEFT JOIN fud30_mod AS mo \n" .
-                     "   ON mo.user_id='{$uid}' AND mo.forum_id='{$rid}' \n".
-                     " LEFT JOIN fud30_group_cache AS g2 \n".
-                     "   ON g2.user_id='{$uid}' AND g2.resource_id='{$rid}' \n".
-                     " LEFT JOIN fud30_users AS usr \n ".
-                     "   ON usr.id='{$uid}' ";
-
-        $selects = " g1.group_cache_opt ";
-        if( $uid )
-            $selects = " usr.users_opt, mo.id AS is_mod, COALESCE(g2.group_cache_opt, g1.group_cache_opt) AS group_cache_opt ";
-
-        $qStr = " SELECT {$selects} \n".
-                " FROM fud30_group_cache AS g1 \n".
-                " {$joins} \n" .
-                " WHERE ( g1.user_id  = '{$id2}' AND g1.resource_id = '{$rid}' ) \n";
-        $q = $this->CI->db->query( $qStr );
-
-		if( $q->num_rows() == 1 )
-        {
-            $r = $q->first_row();
-            if( ( isset($r->users_opt) AND ($r->users_opt & FUD_users_opt::IS_ADMIN) ) OR
-                ( isset($r->is_mod) AND null != ($r->is_mod) ) OR
-                ( $r->group_cache_opt & FUD_groups_opt::VISIBLE) )
-                return TRUE;
-        }
-
-        return FALSE;
+      return FALSE;
     }
+
+    $id2 = $uid ? 2147483647 : 0;
+
+    $joins = "";
+    if( $uid )
+    {
+      $joins = "LEFT JOIN {$prefix}mod AS mo 
+                  ON mo.user_id='{$uid}' AND mo.forum_id='{$rid}' 
+                LEFT JOIN {$prefix}group_cache AS g2
+                  ON g2.user_id='{$uid}' AND g2.resource_id='{$rid}'
+                LEFT JOIN {$prefix}users AS usr
+                  ON usr.id='{$uid}' ";
+    }
+    
+    $selects = " g1.group_cache_opt ";
+    if( $uid )
+    {
+      $selects = " usr.users_opt, mo.id AS is_mod, 
+                   COALESCE(g2.group_cache_opt, g1.group_cache_opt) 
+                     AS group_cache_opt ";
+    }
+
+    $qStr = " SELECT {$selects}
+              FROM {$prefix}group_cache AS g1
+              {$joins}
+              WHERE ( g1.user_id  = '{$id2}' AND g1.resource_id = '{$rid}' ) ";
+    $q = $this->CI->db->query( $qStr );
+
+    if( count( $q->result_array() ) == 1 )
+    {
+      $r = $q->row();
+      if( ( isset($r->users_opt) AND ($r->users_opt & FUD_users_opt::IS_ADMIN) ) OR
+          ( isset($r->is_mod) AND null != $r->is_mod) OR
+          ($r->group_cache_opt & $permission) )
+      return TRUE;
+    }
+
+    return FALSE;
+  }
+
+  function forum_is_visible( $rid, $uid = 0 )
+  {
+    $prefix = $GLOBALS['DBHOST_TBL_PREFIX'];
+    $id2 = $uid ? 2147483647 : 0;
+
+    $joins = "";
+    if( $uid )
+    {
+      $joins = "LEFT JOIN {$prefix}mod AS mo
+                  ON mo.user_id='{$uid}' AND mo.forum_id='{$rid}'
+                LEFT JOIN {$prefix}group_cache AS g2
+                  ON g2.user_id='{$uid}' AND g2.resource_id='{$rid}'
+                LEFT JOIN {$prefix}users AS usr
+                  ON usr.id='{$uid}' ";
+    }
+
+    $selects = " g1.group_cache_opt ";
+    if( $uid )
+    {
+      $selects = "usr.users_opt, mo.id AS is_mod, 
+                  COALESCE(g2.group_cache_opt, g1.group_cache_opt) 
+                  AS group_cache_opt ";
+    }
+
+    $qStr = " SELECT {$selects}
+              FROM {$prefix}group_cache AS g1
+              {$joins}
+              WHERE ( g1.user_id='{$id2}' AND g1.resource_id='{$rid}' ) ";
+    $q = $this->CI->db->query( $qStr );
+
+    if( $q->num_rows() == 1 )
+    {
+      $r = $q->first_row();
+      if( ( isset($r->users_opt) AND ($r->users_opt & FUD_users_opt::IS_ADMIN) ) OR
+          ( isset($r->is_mod) AND null != ($r->is_mod) ) OR
+          ( $r->group_cache_opt & FUD_groups_opt::VISIBLE) )
+      return TRUE;
+    }
+
+    return FALSE;
+  }
 }
 
 /* End of file fud_library.php */
