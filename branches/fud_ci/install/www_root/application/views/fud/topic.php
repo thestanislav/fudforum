c3c017a9f81cc8b6786b7c5349fd7b6be95e09a0
theonlynexus <theonlynexus@75f985d5-eef5-437a-b917-4a5afe5ebf4a>
2015-02-27 17:28:30 +0000

    Trying new look for messages in topic, starting to rename CSS classes using fud- prefix, replacing underscores with dashes

--- branches/fud_ci/install/www_root/application/views/fud/topic.php
+++ branches/fud_ci/install/www_root/application/views/fud/topic.php
@@ -6,58 +6,52 @@
               <!-- Pagination -->
               {pagination}
               <!-- Contents -->
-              {messages}
-              <div id="post_{m_id}" class="fud_post" >
-                <div class="header">
-                  <div class="width_75 inline_block">
-                    <span class="subject">{m_subject}</span>
-                    <span class="id">[Message #: {m_id}]</span>
+              <div id="messages">
+                {messages}
+                <div id="post_{m_id}" class="fud-message" >
+                  <div class="fud-message-header">
+                    <div class="width_75 inline_block">
+                      <span class="fud-message-subject">{m_subject}</span>
+                      <span class="fud-message-id">[Message #: {m_id}]</span>
+                    </div>
+                    <div class="width_auto inline_block text_right">
+                      <span class="fud-message-date">{m_date}</span>
+                    </div>
                   </div>
-                  <div class="width_auto inline_block text_right">
-                    <span class="date">{m_date}</span>
-                  </div>
-                </div>
-                <div class="author" >
-                  {m_avatar}
-                  <div class="contact_actions inline_block">
+                  <div class="fud-message-author-info" >
                     <div>
+                      {m_avatar}
+                      <div class="inline_block vertical_top">
+                        <span class="fud-message-author">{m_login}</span>
+                      </div>
+                    </div>
+                    <div class="fud-contact-actions">
                       <a class="pure-button button-xsmall" href="#">Profile</a>
                     </div>
-                    <div>
+                    <div class="fud-contact-actions">
                       <a class="pure-button button-xsmall" href="#">PM</a>
-                    </div>
-                    <div>
                       <a class="pure-button button-xsmall" href="#">Email</a>
                     </div>
-                  </span>
-                  </div>
-                  <div class="contact_actions inline_block">
-                    <div>
+                    <div class="fud-contact-actions">
                       <a class="pure-button button-xsmall" href="#">Add buddy</a>
-                    </div>
-                    <div>
                       <a class="pure-button button-xsmall" href="#">Ignore</a>
                     </div>
-                  </span>
                   </div>
-                  <div class="inline_block vertical_top">
-                    <span class="author">{m_login}</span>
-                  </div>                  
+                  <div class="clear fud-message-body">
+                    <span>
+                      {m_body}
+                    </span>
+                  </div>
+                  <div class="fud-message-actions">
+                    <?php if($can_reply): ?>
+                    <a class="pure-button" href="{m_reply_url}">{m_reply_text}</a>
+                    <a class="pure-button" href="{m_quote_url}">{m_quote_text}</a>
+                    <?php endif; ?>
+                  </div>
+                  <div class="clear"></div>
                 </div>
-                <div class="clear body">
-                  <span>
-                    {m_body}
-                  </span>
-                </div>
-                <div class="actions">
-                  <?php if($can_reply): ?>
-                  <a class="pure-button" href="{m_reply_url}">{m_reply_text}</a>
-                  <a class="pure-button" href="{m_quote_url}">{m_quote_text}</a>
-                  <?php endif; ?>
-                </div>
-                <div class="clear"></div>
+                {/messages}
               </div>
-              {/messages}
               <!-- End Contents -->
             </div>
           </div>
