dfe87f99ff03e812f326231afd76036ece7f9c04
theonlynexus <theonlynexus@75f985d5-eef5-437a-b917-4a5afe5ebf4a>
2015-02-27 05:25:58 +0000

    Correct use of pure-menu for site navigation

--- branches/fud_ci/install/www_root/application/views/fud/html_head.php
+++ branches/fud_ci/install/www_root/application/views/fud/html_head.php
@@ -1,7 +1,6 @@
 <!-- TODO(nexus): generate template tags for theme etc -->
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
-<link rel="stylesheet" type="text/css" 
-      href="{base_url}theme/default/pure-min.css" >
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
 <link rel="stylesheet/less" type="text/css" 
       href="{base_url}theme/default/theme.css" >
 <script src="{base_url}js/jquery.min.js" ></script>
--- branches/fud_ci/install/www_root/application/views/fud/site_navigation.php
+++ branches/fud_ci/install/www_root/application/views/fud/site_navigation.php
@@ -1,14 +1,14 @@
-        <nav id="fud_site_navigation">
-          <ul>
-            <li><a class="pure-button" href="{home_url}">Home</a></li>
-            <li><a class="pure-button" href="#">Members</a></li>
-            <li><a class="pure-button" href="#">Calendar</a></li>
-            <li><a class="pure-button" href="#">Search</a></li>
-            <li><a class="pure-button" href="#">Help</a></li>
-            <li><a class="pure-button" href="{cp_or_register_url}">{cp_or_register_text}</a></li>
-            <li><a class="pure-button" href="{login_logout_url}">{login_logout_text}</a></li>
+        <nav id="fud_site_navigation" class="pure-menu pure-menu-horizontal">
+          <ul class="pure-menu-list" >
+            <li class="pure-menu-item"><a class="pure-menu-link" href="{home_url}">Home</a></li>
+            <li class="pure-menu-item"><a class="pure-menu-link" href="#">Members</a></li>
+            <li class="pure-menu-item"><a class="pure-menu-link" href="#">Calendar</a></li>
+            <li class="pure-menu-item"><a class="pure-menu-link" href="#">Search</a></li>
+            <li class="pure-menu-item"><a class="pure-menu-link" href="#">Help</a></li>
+            <li class="pure-menu-item"><a class="pure-menu-link" href="{cp_or_register_url}">{cp_or_register_text}</a></li>
+            <li class="pure-menu-item"><a class="pure-menu-link" href="{login_logout_url}">{login_logout_text}</a></li>
             <?php if( !empty($administration_url) ): ?>
-            <li><a class="pure-button" href="{administration_url}">{administration_text}</a></li>
+            <li class="pure-menu-item"><a class="pure-menu-link" href="{administration_url}">{administration_text}</a></li>
             <?php endif; ?>
           </ul>
         </nav>
