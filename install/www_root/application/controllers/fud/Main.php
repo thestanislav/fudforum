246cf9f1020fceab20095396548d2de7d08a3e3f
theonlynexus <theonlynexus@75f985d5-eef5-437a-b917-4a5afe5ebf4a>
2015-03-04 05:34:54 +0000

    Added TTF font for captcha, set colors and font size to be human readeable but hard to process. TODO: Still need to add crete the captcha table during install, still need logic to verify the captcha.

--- install/www_root/application/controllers/fud/Main.php
+++ install/www_root/application/controllers/fud/Main.php
@@ -339,20 +339,20 @@ class Main extends CI_Controller
         //'word'          => 'Random word',
         'img_path'      => './captcha/',
         'img_url'       => base_url('captcha/'),
-        //'font_path'     => './path/to/fonts/texb.ttf',
-        'img_width'     => 150,
-        'img_height'    => 30,
+        'font_path'     => './fonts/AnkaCoder-C75-r.ttf',
+        'img_width'     => 200,
+        'img_height'    => 50,
         'expiration'    => 7200,
-        'word_length'   => 4,
-        'font_size'     => 24,
-        'img_id'        => 'Imageid',
-        'pool'          => '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        'word_length'   => 6,
+        'font_size'     => 18,
+        'img_id'        => 'FUDcaptcha',
+        'pool'          => '0123456789abcdefghijkmnpqrstuvxyz',
         // White background and border, black text and red grid
         'colors'        => array(
           'background' => array(255, 255, 255),
           'border' => array(255, 255, 255),
-          'text' => array(255, 60, 60),
-          'grid' => array(255, 40, 40)
+          'text' => array(200, 0, 0),
+          'grid' => array(255, 0, 0)
         )
     );
     
