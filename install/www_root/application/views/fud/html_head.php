b872de6058022cfe521d47470f2a5cf4afbba03c
theonlynexus <theonlynexus@75f985d5-eef5-437a-b917-4a5afe5ebf4a>
2015-03-16 02:08:20 +0000

    Added viewport meta element to html_head.php

--- install/www_root/application/views/fud/html_head.php
+++ install/www_root/application/views/fud/html_head.php
@@ -1,5 +1,6 @@
 <!-- TODO(nexus): generate template tags for theme etc -->
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
 <link rel="stylesheet/less" type="text/css" 
       href="{base_url}theme/default/theme.css" >
