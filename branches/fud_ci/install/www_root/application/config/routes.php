c9e7d2c0f613e5ddfe3f870b945bb5282e8de793
theonlynexus <theonlynexus@75f985d5-eef5-437a-b917-4a5afe5ebf4a>
2015-07-13 02:10:47 +0000

    Started refactoring controllers, currently not working because I need to switch computers

--- branches/fud_ci/install/www_root/application/config/routes.php
+++ branches/fud_ci/install/www_root/application/config/routes.php
@@ -67,6 +67,16 @@ $route['forum/(:num)/(:num)/(:num)/(:num)'] = "fud/main/forum/$1/$2/$3/$4";
 $route['topic/(:num)/(:num)/(:num)'] = "fud/main/topic/$1/$2/$3";
 $route['topic/(:num)/(:num)/(:num)/(:num)'] = "fud/main/topic/$1/$2/$3/$4";
 $route['topic/(:num)/(:num)/(:num)/(:num)/(:num)'] = "fud/main/topic/$1/$2/$3/$4/$5";
+
+$route['message/get/(:num)'] = "fud/message/get/$1";
+$route['message/new/(:num)'] = "fud/message/new/$1";
+$route['message/new/(:num)/(:num)'] = "fud/messaeg/new/$1/$2";
+$route['message/new/(:num)/(:num)/(:num)'] = "fud/message/new/$1/$2/$3";\
+$route['message/edit/(:num)'] = "fud/message/edit/$1";
+$route['message/delete/(:num)'] = "fud/message/delete/$1/";
+
+
+
 $route['reply/(:num)'] = "fud/main/reply/$1";
 $route['reply/(:num)/(:num)'] = "fud/main/reply/$1/$2";
 $route['reply/(:num)/(:num)/(:num)'] = "fud/main/reply/$1/$2/$3";
